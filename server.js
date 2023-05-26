47eb216ec52849c33d2a0f7d3f48e6cc0ed46564
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-26 11:40:11 +0800

    Added the main template for the server
    need to add the logger and the router and controller for the route
    
    also need to add the filler html files

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middlewawre/errorHandler");
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res, sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

58207b546e2e99d5690dbdbaa0e67906cb30d442
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-27 13:12:28 +0800

    Created the login function
    similar to register function

--- server.js
+++ server.js
@@ -14,16 +14,17 @@ app.use(express.json());
 
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
 
 app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res, sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ error: "404 Not Found" });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
+    res.status(404);
+    if (req.accepts("html")) {
+        res, sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type("txt").send("404 Not Found");
+    }
 });
 
 app.use(errorHandler);

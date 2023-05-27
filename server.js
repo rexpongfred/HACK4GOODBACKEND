84577066b2177c6689ec82960c85914b4e5fd377
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-27 16:28:48 +0800

    Added the controller and route for refreshing token
    cookie does not work if the option is secure, need to enable some other stuff

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(cookieParser());
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
 
 app.use(verifyJWT);
 app.use("/secret", require("./routes/api/secretaccess"));

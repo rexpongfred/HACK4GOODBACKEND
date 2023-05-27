0788d54a2f90b39882f284d5ae25cf337d0d044a
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-27 14:41:06 +0800

    Integrated the login with the JWT
    JWT will be required to access a test secret html page, yet to implement the logic for the refresh token

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
@@ -19,9 +21,15 @@ app.use(express.urlencoded({ extended: false }));
 // built-in middleware for json
 app.use(express.json());
 
+// middleware for cookies
+app.use(cookieParser());
+
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+
+app.use(verifyJWT);
+app.use("/secret", require("./routes/api/secretaccess"));
 
 app.all("*", (req, res) => {
     res.status(404);

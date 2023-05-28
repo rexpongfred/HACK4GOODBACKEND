3e4e482ff2d3fb6685b8b4ea11f514d7fcdc5e42
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-28 20:39:37 +0800

    Revert "Update allowedOrigins.js"
    This reverts commit c0ffcde15c4d9962f28a3a286b6504688832710c.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     "https://mysite.com",
     "something",
     "http://localhost:3000",
-    "https://helpful-malasada-4d69b0.netlify.app",
+    "https://helpful-malasada-4d69b0.netlify.app/",
 ];
 
 module.exports = allowedOrigins;

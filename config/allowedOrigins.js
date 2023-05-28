e92a29e870ed5e194573ffbed2c43f4475d00894
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-28 21:36:08 +0800

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
     "https://mysite.com",
-    "something",
     "http://localhost:3000",
-    "https://helpful-malasada-4d69b0.netlify.app/",
+    "https://helpful-malasada-4d69b0.netlify.app/login",
+    "https://helpful-malasada-4d69b0.netlify.app",
 ];
 
 module.exports = allowedOrigins;

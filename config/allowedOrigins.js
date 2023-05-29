4d1855dfd7fd12fedf0b0a3d6a53486ac5398a0c
Frederick Pua <puachengxuan.fp@gmail.com>
2023-05-29 12:45:26 +0800

    updated to allow frontend cors access

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     "http://localhost:3000",
     "https://helpful-malasada-4d69b0.netlify.app/login",
     "https://helpful-malasada-4d69b0.netlify.app",
+    "https://cyfer.netlify.app",
 ];
 
 module.exports = allowedOrigins;

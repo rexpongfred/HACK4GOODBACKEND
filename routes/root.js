10f26902c0a3fead621b818ad7453235442363b6
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-26 13:38:46 +0800

    dsafs

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+// const path = require("path");
 
 router.get("^/$|index(.html)?", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+    // res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+    fetch(
+        `https://sync-testnet.vechain.org/transactions/0xe815175e54e37000ad79c73b111c3711d4987c8efeb8d2ef30876cbcdc758516/receipt`
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            res.json(data);
+        });
 });
 
 module.exports = router;
